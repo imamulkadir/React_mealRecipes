e3114ab14167fcec154911a4ef3c64ba5a522cac
Imamul Kadir <imamulkadiroyan@gmail.com>
2025-09-01 08:18:17 +0600

    updated visuals and contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact Us</div>;
+  return (
+    <div className="max-w-xl mx-auto mt-16 p-8 bg-white rounded-lg shadow-lg flex flex-col items-center gap-4">
+      <h1 className="text-3xl font-bold text-gray-800 mb-2">Contact Us</h1>
+      <p className="text-lg text-gray-600 text-center ">
+        For any questions or suggestions, please email us at{" "}
+        <a
+          href="mailto:imamulkadiroyan@gmai.com"
+          className="font-bold hover:text-amber-400"
+        >
+          imamulkadiroyan@gmai.com
+        </a>
+        .
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
